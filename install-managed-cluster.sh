78323e0a6858954dcb00feb03df17b044b9ab2a1
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-02-24 13:09:56 -0500

    Update install-managed-cluster.sh

--- install-managed-cluster.sh
+++ install-managed-cluster.sh
@@ -24,3 +24,25 @@ oc apply -f managed-cluster-$clustername.yaml
 
 oc get secret ${CLUSTER_NAME}-import -n ${CLUSTER_NAME} -o jsonpath={.data.crds\\.yaml} | base64 --decode > klusterlet-crd.yaml
 oc get secret ${CLUSTER_NAME}-import -n ${CLUSTER_NAME} -o jsonpath={.data.import\\.yaml} | base64 --decode > import.yaml
+
+echo << EOF > klusterlet-addon-config.yaml
+apiVersion: agent.open-cluster-management.io/v1
+kind: KlusterletAddonConfig
+metadata:
+  name: <cluster_name>
+  namespace: <cluster_name>
+spec:
+  applicationManager:
+    enabled: true
+  certPolicyController:
+    enabled: true
+  iamPolicyController:
+    enabled: true
+  policyController:
+    enabled: true
+  searchCollector:
+    enabled: true
+  EOF
+  oc apply -f klusterlet-addon-config.yaml
+  oc get pod -n open-cluster-management-agent-addon
+    
